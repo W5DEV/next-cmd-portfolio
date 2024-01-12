f4ab5ef5e888292f0cd08838e052312b22a65fe6
John Larson <jl@buildmidwestern.com>
2024-01-12 04:48:45 -0600

    feat: implemented commands, history

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import CopyrightHeadng from "./components/CopyrightHeading";
 import HelpText from "./components/HelpText";
 import Input from "./components/Input";
@@ -8,11 +8,326 @@ import MorseClosing from "./components/MorseEnding";
 import MorseOpening from "./components/MorseOpening";
 import Prefix from "./components/Prefix";
 import WelcomeArt from "./components/WelcomeArt";
+import {
+  whois,
+  whoami,
+  social,
+  secret,
+  projects,
+  help,
+} from "./scripts/commands";
 
 export default function Home() {
-  const [history, setHistory] = useState<ReactNode>([]);
+  const [history, setHistory] = useState<ReactNode[]>([]);
+  const [isEditing, setEditing] = useState(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [password, setPassword] = useState<boolean>(false);
+  const [cmdHistory, setCmdHistory] = useState<ReactNode[]>([]);
+  const inputRef = useRef(null);
+  const toggleEditing = () => {
+    setEditing(!isEditing);
+  };
+
+  const addHistory = (command: string) => {
+    console.log(password, command);
+    setInputValue("");
+
+    if (password) {
+      if (command.toLowerCase() === "w5devpw") {
+        setHistory([
+          ...history,
+          <div
+            className="flex flex-col items-start justify-center"
+            key={Math.random()}
+          >
+            <div className="w-full flex flex-row justify-start items-center">
+              <Prefix />
+              <span className="">Password: *****</span>
+            </div>
+            <div className="p-4">{secret}</div>
+          </div>,
+        ]);
+        setPassword(false);
+      } else {
+        setHistory([
+          ...history,
+          <div
+            className="flex flex-col items-start justify-center"
+            key={Math.random()}
+          >
+            <div className="w-full flex flex-row justify-start items-center">
+              <Prefix />
+              <span className="">Password: *****</span>
+            </div>
+            <div className="p-4">Password Invalid!</div>
+          </div>,
+        ]);
+      }
+    } else {
+      switch (command.toLowerCase()) {
+        case "banner":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <WelcomeArt />
+            </div>,
+          ]);
+          break;
+
+        case "clear":
+          setHistory([]);
+          setCmdHistory([]);
+          break;
+
+        case "email":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+            </div>,
+          ]);
+          setTimeout(function () {
+            window.open("mailto:hello@w5dev.com");
+          });
+          break;
+
+        case "github":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+            </div>,
+          ]);
+          setTimeout(function () {
+            window.open("https://github.com/JLarsonMWI");
+          }, 1000);
+          break;
+
+        case "help":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{help}</div>
+            </div>,
+          ]);
+          break;
+
+        case "history":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{cmdHistory}</div>
+            </div>,
+          ]);
+          break;
+
+        case "linkedin":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+            </div>,
+          ]);
+          setTimeout(function () {
+            window.open("https://linkedin.com/in/johnjlarson");
+          }, 1000);
+          break;
+
+        case "projects":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{projects}</div>
+            </div>,
+          ]);
+          break;
+
+        case "secret":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+            </div>,
+          ]);
+          setPassword(true);
+          break;
+
+        case "social":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{social}</div>
+            </div>,
+          ]);
+          break;
+
+        case "sudo":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">Oh no! You&apos;re not admin!</div>
+            </div>,
+          ]);
+          setTimeout(function () {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+          }, 1000);
+          break;
+
+        case "whoami":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{whoami}</div>
+            </div>,
+          ]);
+          break;
+
+        case "whois":
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="w-full flex flex-row justify-start items-center">
+                <Prefix />
+                <span className="">{command}</span>
+              </div>
+              <div className="p-4">{whois}</div>
+            </div>,
+          ]);
+          break;
+
+        default:
+          setHistory([
+            ...history,
+            <div
+              className="flex flex-col items-start justify-center"
+              key={Math.random()}
+            >
+              <div className="flex flex-row items-center justify-start">
+                <Prefix />
+                <span>{command}</span>
+              </div>
+              <div className="flex flex-col items-center justify-start py-2 pl-4">
+                <p className="text-green-500">
+                  &apos;<i className="text-cyan-600">{command}</i>&apos; is not
+                  a valid command.{" "}
+                  <b className="font-normal text-yellow-600">
+                    For a list of available commands, type{" "}
+                  </b>
+                  <b className="text-cyan-600 text-shadow-sm shadow-cyan-400 font-normal">
+                    &apos;help&apos;
+                  </b>
+                  .
+                </p>
+              </div>
+            </div>,
+          ]);
+          break;
+      }
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setCmdHistory([
+        ...cmdHistory,
+        <div key={Math.random()}>{inputValue}</div>,
+      ]);
+      addHistory(event.currentTarget.value);
+    }
+
+    if (event.key === "Escape") {
+      setPassword(false);
+      setInputValue("");
+    }
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      (inputRef.current as HTMLInputElement)?.focus();
+    }
+  }, [isEditing]);
+
   return (
-    <main className="flex min-h-screen min-w-[700px] flex-col items-start justify-start p-4 text-cyan-600 font-mono gap-2 tracking-widest border-4 border-double border-stone-400 outline-dashed outline-stone-400 -outline-offset-8">
+    <main
+      onClick={toggleEditing}
+      className="flex min-h-screen min-w-[700px] flex-col items-start justify-start p-4 text-cyan-600 font-mono gap-2 tracking-widest border-4 border-double border-stone-400 outline-dashed outline-stone-400 -outline-offset-8"
+    >
       <div className="flex flex-row text-stone-400 text-xs">
         <MorseOpening />
       </div>
@@ -31,13 +346,18 @@ export default function Home() {
       <div className="flex flex-col">{history}</div>
       <div className="relative flex flex-row w-full">
         <Prefix />
+        <span className={password ? "flex" : "hidden"}>Password: </span>
         <input
-          type="text"
+          ref={inputRef}
+          onKeyDown={(event) => handleKeyPress(event)}
+          onChange={(event) => setInputValue(event.currentTarget.value)}
+          value={inputValue}
+          type={password ? "password" : "text"}
           className="w-full bg-transparent border-none focus:border-none focus:outline-none focus:ring-0"
         />
-        <div className="absolute l-10 w-full">
-          <b className="animate-caret">█</b>
-        </div>
+        {/* <div className="absolute l-10 w-full">
+            <b className="animate-caret">█</b>
+          </div> */}
       </div>
     </main>
   );
---
+++ app/scripts/commands.tsx
@@ -0,0 +1,175 @@
+const password = "w5dev";
+const linkedin = "https://www.linkedin.com/in/johnjlarson/";
+const github = "https://github.com/JLarsonMWI/";
+const email = "mailto:hello@w5dev.com";
+
+export const whois = (
+  <div className="flex flex-col justify-start items-start w-full text-pretty">
+    <br />
+    <span>Howdy, I&apos;m John (aka W5DEV)! 🤠</span>
+    <span>
+      I am a software developer for Midwestern Interactive and I build websites
+      and web applications much like this one.
+    </span>
+    <span>
+      I am a self-taught developer and I have been coding professionally for
+      about 10 years, starting with simple websites using Wordpress and vanilla
+      HTML/CSS, and eventually working my way up to full stack web applications
+      using frontend libraries and frameworks like React, NextJS, Vue.js, and
+      backend frameworks and languages like NestJS, Kotlin and Laravel.
+    </span>
+    <span>
+      My hobbies outside of programming include hiking, camping, survival,
+      shooting and ham radio.
+    </span>
+    <span>
+      Professionally, my story started with jobs in IT and software support,
+      then progressed to engineering roles, and finally to software development.
+      I have worked for a variety of companies - both small and large - in a
+      wide array of industries, and I have experience with both customer-facing
+      and internal-only roles.
+    </span>
+    <span>
+      Outside of company-based work, my freelance work has included running a
+      successful web-first sports media company (South MS Sports, era
+      2013-2018), working with local businesses to improve their online
+      footprint, and building custom web applications to solve problems in my
+      own day-to-day life.
+    </span>
+    <br />
+  </div>
+);
+export const whoami = (
+  <div className="flex flex-col justify-start items-start w-full">
+    <br />
+    <span>
+      The paradox of “Who am I?” is: we never know, but, we constantly find out.
+    </span>
+    <br />
+  </div>
+);
+
+export const social = (
+  <div className="flex flex-col justify-center items-start w-full">
+    <br />
+    <div className="flex flex-row w-full">
+      <span className="w-48 flex flex-row">linkedin</span>
+      <span>
+        <a
+          className="cursor-pointer hover:underline"
+          href={linkedin}
+          target="_blank"
+        >
+          linkedin.com/in/johnjlarson
+        </a>
+      </span>
+    </div>
+    <div className="flex flex-row w-full">
+      <span className="w-48">github</span>
+      <span>
+        <a
+          className="cursor-pointer hover:underline"
+          href={github}
+          target="_blank"
+        >
+          github.com/JLarsonMWI
+        </a>
+      </span>
+    </div>
+    <br />
+  </div>
+);
+
+export const secret = (
+  <div className="flex flex-col justify-center items-start">
+    <br />
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        sudo
+      </span>{" "}
+      <span>Only use if you&apos;re admin</span>
+    </div>
+    <br />
+  </div>
+);
+
+export const projects = (
+  <div>
+    <br />
+    <span>
+      Still curating... most projects are offline, on GitHub, or confidential.
+    </span>
+    <br />
+  </div>
+);
+
+export const help = (
+  <div className="flex flex-col justify-center items-start">
+    <br />
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        banner
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Display the header</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        clear
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Clear terminal</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        email
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Do not email me</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        help
+      </span>
+      <span className="text-nowrap clamp-1 truncate">
+        You obviously already know what this does
+      </span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        history
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">View command history</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        projects
+      </span>
+      <span className="text-nowrap clamp-1 truncate">View coding projects</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        secret
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Find the password</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        social
+      </span>
+      <span className="text-nowrap clamp-1 truncate">
+        Display social networks
+      </span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        whoami
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Who are you?</span>
+    </div>
+    <div className="flex flex-row justify-between items-center">
+      <span className="w-48 text-cyan-600 text-shadow-sm shadow-cyan-400">
+        whois
+      </span>{" "}
+      <span className="text-nowrap clamp-1 truncate">Who is W5DEV?</span>
+    </div>
+    <br />
+  </div>
+);
