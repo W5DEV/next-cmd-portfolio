a66d573131b5010184dc6a64fdcbb726b5337cea
John Larson <jl@buildmidwestern.com>
2024-01-13 20:49:57 -0600

    feat: added projects template for project commands

---
+++ app/scripts/projects.tsx
@@ -0,0 +1,154 @@
+export const electricWorksInc = {
+  name: "Electric Works Inc",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["PHP", "HTML", "CSS", "JavaScript", "jQuery", "Laravel", "WinterCMS"],
+};
+
+export const paulJackson = {
+  name: "Paul Jackson",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: [
+    "PHP",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "AlpineJS",
+    "jQuery",
+    "Laravel",
+    "WinterCMS",
+    "TailwindCSS",
+  ],
+};
+
+export const msmiga = {
+  name: "MS Insurance Guarantee Association",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: [
+    "PHP",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "jQuery",
+    "Laravel",
+    "WinterCMS",
+    "TailwindCSS",
+  ],
+};
+
+export const birminghamAirport = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const youDotCom = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const agerpoint = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const incFile = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const devRev = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const recipeVaultOnline = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const recipeVaultOnlinev2 = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const recipeApi = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const greatIdeaMediaPlatform = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const myBudgetBuddy = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const portfolioV1 = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const terminaPortfolio = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const amazonClone = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
+
+export const ktorMediaApi = {
+  name: "Birmingham Airport",
+  type: "Professional",
+  description: "Put a description here dummy",
+  github: null,
+  tech: ["Gatsby", "GraphQL", "TailwindCSS"],
+};
