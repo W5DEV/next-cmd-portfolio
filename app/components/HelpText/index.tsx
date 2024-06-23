a27c8c0362e58633e379b83c224a2f67d4869d08
John Larson <jl@buildmidwestern.com>
2024-06-23 03:28:25 -0500

    updated contact link

--- app/components/HelpText/index.tsx
+++ app/components/HelpText/index.tsx
@@ -1,6 +1,14 @@
 export default function HelpText() {
   return (
     <div className="flex flex-col gap-2 items-start justify-center bg-transparent tracking-widest">
+      <a
+        href="https://contact.greatidea.dev/"
+        target="_blank"
+        className="text-pink-500 italic text-xs hover:underline"
+      >
+        To reach out regarding 'Legends and Legacies', click here.
+      </a>
+
       <span className="text-yellow-600">
         For a list of available commands, type{" "}
         <span className="text-cyan-600 text-shadow-sm shadow-cyan-400">
